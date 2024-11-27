bce90a2cf6ba52b96f74446a0edca35caa1c8a60
Yeong-huns <vosxja1@gmail.com>
2024-11-28 03:25:57 +0900

    study @OneToOne Relationship

--- netflix/src/movie/entity/movie.entity.ts
+++ netflix/src/movie/entity/movie.entity.ts
@@ -1,18 +1,15 @@
-import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn} from "typeorm";
+import {Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import {BaseEntity} from "./base.entity";
+import {MovieDetail} from "./movie-detail.entity";
 
-export abstract class BaseEntity{
-    @CreateDateColumn()
-    createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
+// ManyToOne  // Director -> 감독은 여러개의 영화를 가질 수 있음
+// OneToOne // MovieDetail -> 영화는 하나의 상세 내용을 가질 수 있음
+// ManyToMany // Genre -> 영화는 여러개의 장르를 가질 수 있고 장르는 여러개의 영화에 속할 수 있음
 
-    @VersionColumn()
-    version: number;
-}
 
 @Entity()
-export class Movie extends BaseEntity{
+export class Movie extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
@@ -21,4 +18,8 @@ export class Movie extends BaseEntity{
 
     @Column()
     genre: string;
+
+    @OneToOne(() => MovieDetail)
+    @JoinColumn()
+    detail: MovieDetail;
 }
